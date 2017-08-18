0e6c44dc8154ec1d594017e3e44f197bb2d90eb8
Jack Leigh <jack.leigh@gmail.com>
2017-08-18 17:13:17 +0100

    Enable flow

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   extends: 'standard',
   // required to lint *.vue files
   plugins: [
+    'flowtype-errors',
     'html'
   ],
   // add your custom rules here
@@ -22,6 +23,7 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'flowtype-errors/show-errors': 'error',
   }
 }
